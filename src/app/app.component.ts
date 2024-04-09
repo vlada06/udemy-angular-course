cd1b8a3a7e2f95cc2607a59bf4d9f17fd601bb7b
Vlada <vladimir.davidovic@thoughtworks.com>
2024-04-09 16:51:33 +0100

    initial commit
    longDescription (What: * Why: * How: * Side Effects: *)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'udemy-angular-course';
+  data = {
+    title: '<h1>Udemy Angular Course</h1><script>alert("simulated JS attack")</script>'
+  }
+
+  inIconClick() {
+    alert("klikchy, orle");
+  }
+
+  onKeyUp(newTitle: string){
+    alert("ene, strelica, sokole");
+  }
+  protected readonly onkeyup = onkeyup;
 }
