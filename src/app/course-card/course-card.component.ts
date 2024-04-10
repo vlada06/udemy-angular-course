1bc07110c6f1819e06680a44bf582ca254497778
Vlada <vladimir.davidovic@thoughtworks.com>
2024-04-10 15:51:21 +0100

    001: Component inputs
    Fiddling with titles, data etc.

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  standalone: true,
+  styleUrl: './course-card.component.css'
+})
+
+export class CourseCardComponent {
+
+  title: string = "";
+
+  // constructor() {
+  // }
+  //
+  // ngOnInit() {
+  // }
+
+}
