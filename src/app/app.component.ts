92b43ebf1b7f3822b0c65801a4bffb4b67084523
Vlada <vladimir.davidovic@thoughtworks.com>
2024-04-10 15:51:21 +0100

    001: Component inputs
    Fiddling with titles, data etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {NgOptimizedImage} from "@angular/common";
+import {CourseCardComponent} from "./course-card/course-card.component";
+import {COURSES} from "../db-data";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage],
+  imports: [
+    RouterOutlet,
+    NgOptimizedImage,
+    CourseCardComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data = {
-    title: '<h1>Udemy Angular Course</h1><script>alert("simulated JS attack")</script>'
-  }
-
-  inIconClick() {
-    alert("klikchy, orle");
-  }
-
-  onKeyUp(newTitle: string){
-    alert("ene, strelica, sokole");
-  }
-  protected readonly onkeyup = onkeyup;
+  coreCourse = COURSES[0];
+  rxjsCourse = COURSES[1];
+  ngrxCourse = COURSES[2];
 }
