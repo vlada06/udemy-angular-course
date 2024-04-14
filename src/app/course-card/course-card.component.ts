5f7586348a9a9874889f5b86cc10008112627992
Vlada <vladimir.davidovic@thoughtworks.com>
2024-04-14 10:57:17 +0100

    Start somewhere

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Course} from "../model/course";
+import {COURSES} from "../../db-data";
+
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  standalone: true,
+  styleUrl: './course-card.component.css'
+})
+
+export class CourseCardComponent {
+
+  @Input({
+    required: true
+  })
+  course: Course | any;
+
+  @Output(courseSelected)
+  courseEmitter = new EventEmitter<Course> ();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onCourseViewed(){
+    console.log("card component - button clicked...");
+    this.courseEmitter.emit(this.course);
+  }
+}
